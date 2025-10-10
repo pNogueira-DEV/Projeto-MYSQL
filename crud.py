1ba1a04e8cba0687bb1ae3dc9ea3d428d9790201
Paulo Ricardo <pauloricardo.st1812@hotmail.com>
2025-10-10 14:38:33 -0300

    fução listar alunos

--- crud.py
+++ crud.py
@@ -14,4 +14,23 @@ def inserir_aluno(nome_aluno, idade_aluno):
         finally:
             cursor.close()
             conexao.cloese()
+
+def listar_aluno():
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+                "SELECT * FROM alunos ORDER BY ID"
+            )
+            return cursor.fetchall()
+        except Exception as erro:
+            print(f"Erro ao tentar exibir aluno: {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
+lista = listar_aluno()
+for linha in lista:
+    print(f" ID: {linha[0]} - NOME: {linha[1]} - IDADE: {linha[2]}")
+
             
